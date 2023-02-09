9150ebc5476705d73424d7421ffaf163cc4298e4
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-02-09 18:31:48 +0100

    grouped all private routes

--- routes/web.php
+++ routes/web.php
@@ -21,23 +21,7 @@ Route::get('/', [MainController::class, 'home']) -> name('home');
 // Show route
 Route::get('/project/show/{project}', [MainController::class, 'show']) -> name('project.show');
 
-// Destroy route
-Route::get('/project/delete/{project}', [MainController::class, 'destroy']) -> name('project.destroy');
 
-// Create route
-Route::get('/project/create', [MainController::class, 'create']) -> name('project.create');
-
-// Store route
-Route::post('/project/store', [MainController::class, 'store']) -> name('project.store');
-
-// Edit route
-Route::get('/project/edit/{project}', [MainController::class, 'edit']) -> name('project.edit');
-
-// Update route
-Route::post('/project/update/{project}', [MainController::class, 'update']) -> name('project.update');
-
-// After login route
-Route::get('/logged', [MainController::class, 'logged']) -> middleware(['auth', 'verified']) -> name('logged');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -47,6 +31,27 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Grouping private routes
+Route::middleware('auth')->group(function (){
+    // Destroy route
+    Route::get('/project/delete/{project}', [MainController::class, 'destroy']) -> name('project.destroy');
+
+    // Create route
+    Route::get('/project/create', [MainController::class, 'create']) -> name('project.create');
+
+    // Store route
+    Route::post('/project/store', [MainController::class, 'store']) -> name('project.store');
+
+    // Edit route
+    Route::get('/project/edit/{project}', [MainController::class, 'edit']) -> name('project.edit');
+
+    // Update route
+    Route::post('/project/update/{project}', [MainController::class, 'update']) -> name('project.update');
+
+    // After login route
+    Route::get('/logged', [MainController::class, 'logged']) -> name('logged');
 });
 
 require __DIR__.'/auth.php';
