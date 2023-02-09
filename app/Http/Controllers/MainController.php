3aa200d00e520b6d6baa4a6e751813089bc697db
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-02-09 11:17:11 +0100

    linked fake data to frontend through maincontroller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Project;
+
 class MainController extends Controller
 {
     public function home(){
 
-        return view ('pages.home');
+        $projects = Project::orderBy('created_at') -> get();
+
+        return view ('pages.home', compact('projects'));
     }
 
     public function logged(){
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -14,4 +14,24 @@
         </p>
     </a>
 
+    <ul>
+        @foreach ($projects as $project)
+            <li>
+                <h4>
+
+                    {{ $project -> name }}
+                </h4>
+
+                <p>
+                    {{ $project -> description }}
+                </p>
+                
+                <h6>
+                    Released:
+                    {{ $project -> release_date }}
+                </h6>
+            </li>
+        @endforeach
+    </ul>
+
 @endsection
