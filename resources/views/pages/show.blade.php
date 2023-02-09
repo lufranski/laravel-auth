08eefdc5109ad224e5abea45bf626ae908aad6cb
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-02-09 11:50:56 +0100

    added some layout changes in show page

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -2,4 +2,24 @@
 
 @section('content')
     <h3>{{ $project -> name }}</h3>
+
+    <p>
+        {{ 
+            $project -> description
+            ? $project -> description
+            : '- no description available -' 
+        }}
+    </p>
+
+    <h5>
+        Released on: 
+        {{ $project -> release_date }}
+    </h5>
+
+    <h6>
+        follow the link to find out more:
+        <a href="">
+            {{ $project -> repo_link}}
+        </a>
+    </h6>
 @endsection
