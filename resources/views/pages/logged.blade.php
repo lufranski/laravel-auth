70144efb3b4921e212b748df7634abc3d1a5f7e9
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-02-09 12:13:42 +0100

    created form handling new project adding

--- resources/views/pages/logged.blade.php
+++ resources/views/pages/logged.blade.php
@@ -6,15 +6,43 @@
         <h1>admin panel.</h1>
     </section>
 
-    <ul>
-        @foreach ($projects as $project)
-            <li>
-                Project name:
-                {{ $project -> name}}
+    <div>
 
-                <a href="{{ route('project.destroy', $project)}}">DELETE</a>
-            </li>
-        @endforeach
-    </ul>
+        <h5>Current projects.</h5>
+        <ul>
+            @foreach ($projects as $project)
+                <li>
+                    Project name:
+                    {{ $project -> name}}
+    
+                    <a href="{{ route('project.destroy', $project)}}">DELETE</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
+    <div>
+        <h5>Add new project.</h5>
+
+        <form method="POST" action="">
+        
+            <label for="name">Project name.</label>
+            <input type="text" name="name">
+
+            <label for="description">Project description.</label>
+            <textarea name="description" cols="30" rows="10"></textarea>
+
+            <label for="main_image">Project screenshot.</label>
+            <input type="text" name="main_image">
+
+            <label for="release_date">Project released in.</label>
+            <input type="date" name="release_date">
+
+            <label for="repo_link">Project external link.</label>
+            <input type="text" name="repo_link">
+
+            <input type="submit" value="ADD">
+        </form>
+    </div>
 
 @endsection
