2fc9e914a0a903a8b2f1888bc123af210e9fec14
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-02-13 16:24:46 +0100

    fixed img problem

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,7 +12,7 @@
         @foreach ($projects as $project)
             <div class="ms_card">
 
-                <img src="{{ asset('/storage/' . $project -> main_image) }}" alt="">
+                <img src="{{ Vite::asset('storage/app/public/' . $project -> main_image) }}" alt="">
                 <h4>
                     
                     {{ $project -> name }}
